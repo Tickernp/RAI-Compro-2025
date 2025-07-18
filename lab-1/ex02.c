94fb9ea8be04d0d7fcaa713f39d5dded5e68c4b8
Tickernp <ticker0310@gmail.com>
2025-07-18 17:01:28 +0700

    1

---
+++ lab-1/ex02.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{    
+    printf("My name is %s\n", "Naracha Phiriyasrikaew");
+    printf("I am %d yers old\n",18);
+    printf("I am %.2f cm", 186.0);
+    return 0;
+}
---
+++ lab-1/ex06.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() 
+{    
+    printf("Decimal: %d\n",21);
+    printf("Octal: %d\n",25);
+    printf("Hexadecimal: %d\n",15);
+    return 0;
+}
